68a57bad941fe44ccf7fae12701fa344013d199c
CodeCoderCoding <1845371176@qq.com>
2019-05-05 08:41:45 +0800

    change some file

---
+++ app/src/main/java/com/mycompany/androidexample/Content/ContentResolver.java
@@ -0,0 +1,15 @@
+package com.mycompany.androidexample.Content;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mycompany.androidexample.R;
+
+public class ContentResolver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_resolver);
+    }
+}
