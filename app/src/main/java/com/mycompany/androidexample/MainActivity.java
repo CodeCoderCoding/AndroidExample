9c528ea7d912edd9686c6a2f9cf01ce91e7b9c58
CodeCoderCoding <1845371176@qq.com>
2019-05-16 16:04:45 +0800

    change some file

--- app/src/main/java/com/mycompany/androidexample/MainActivity.java
+++ app/src/main/java/com/mycompany/androidexample/MainActivity.java
@@ -1,7 +1,13 @@
 package com.mycompany.androidexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.stetho.Stetho;
+import com.mycompany.androidexample.Storage.SQLiteActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Stetho.initializeWithDefaults(this);
+        Button buttonStorageSqlite=(Button) findViewById(R.id.main_storage_sqlite);
+        buttonStorageSqlite.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this, SQLiteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/mycompany/androidexample/Storage/SQLiteActivity.java
+++ app/src/main/java/com/mycompany/androidexample/Storage/SQLiteActivity.java
@@ -2,14 +2,24 @@ package com.mycompany.androidexample.Storage;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.mycompany.androidexample.R;
 
 public class SQLiteActivity extends AppCompatActivity {
-
+    private MyDatabaseHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sqlite);
+        setContentView(R.layout.storage_sqlite);
+        dbHelper=new MyDatabaseHelper(this,"BookStore.db",null,1);
+        Button createDatabase=(Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                dbHelper.getWritableDatabase();
+            }
+        });
     }
 }
